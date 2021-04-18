4e155cbc17ad44df1e308a55db4fcb448343b6f0
Simon Anil George <Simon@Anils-MBP.domain.name>
2021-04-18 14:50:13 +0530

    delete values from checkbox

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -30,18 +30,32 @@ export class CheckboxComponent implements OnInit {
     console.log(this.checkbox.getRawValue())
   }
 
-  add(e) {
-    console.log(e.target.checked);
-    // this.checkbox.push(this.fb.control(e.target.value))    
+  add(e, i) {
+
+    if (e.target.checked) {
+      this.checkbox.push(this.fb.control(e.target.value))
+      console.log(this.checkbox.getRawValue());
+    }
+    else if (!e.target.checked && this.checkbox.getRawValue().includes(e.target.value)) {
+      let index = this.checkbox.getRawValue().findIndex((check) => {
+        return e.target.value === check;
+
+      })
+      this.checkbox.removeAt(index);
+      console.log(this.checkbox.getRawValue());
+
+    }
+
+
   }
 
-  test() {
+  selectAll() {
     console.log("test")
     this.isActive = !this.isActive;
     if (this.isActive) {
 
 
-    
+
       this.inputs.forEach((input) => {
         this.checkbox.push(this.fb.control(input))
       })
@@ -50,10 +64,11 @@ export class CheckboxComponent implements OnInit {
     else {
       this.checkbox.clear();
     }
-
-
-
     // console.log(this.checkbox.value)
+
+  }
+
+  delete(event) {
 
   }
 }
